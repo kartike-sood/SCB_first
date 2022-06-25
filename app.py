aba8fc2710ce4801ec9f74f882102dbcfa223df2
Kartike Sood <kartikes.ec.19@nitj.ac.in>
2022-06-25 22:06:40 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,70 @@
+from lib2to3.pgen2.pgen import DFAState
+from pickle import MEMOIZE
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
+import pandas as pd
+from pandas_profiling import ProfileReport
+df = 0
+list_of_columns = 0
+app = Flask(__name__)
+
+ALLOWED_EXTENSIONS = {'csv', 'xlsx'}
+UPLOAD_FOLDER = 'static'
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/show_columns", methods=['GET', 'POST'])
+def show_list_of_columns():
+
+    if request.method == "POST":
+        # check if the post request has the file part
+        if 'file1' not in request.files:
+            return render_template('missing_file.html', warning = "I believe you have missed something")
+
+        file = request.files['file1']
+
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            return render_template('missing_file.html', warning = "I believe you have missed something")
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join('static', filename)
+            print(filename, filepath)
+            file.save(filepath)
+
+            global df
+            df = pd.read_csv(filepath)
+            global list_of_columns
+            list_of_columns = df.columns.to_list()
+            
+
+            return render_template("second_page.html", list_of_columns = list_of_columns)
+
+@app.route("/dataReport", methods = ['GET', 'POST'])
+def report():
+    global df
+    # global list_of_columns
+    values = []
+    if request.method == "POST":
+
+        values = request.form.to_dict(flat=False)
+        print(type(values))
+        selected_columns = [value for key, value in values.items()][0]
+        print(selected_columns, "\n")
+        df = df[selected_columns]
+        report = ProfileReport(df, title = "EDA Report", dark_mode = True, html = {'style' : {'full_width' : True}})
+        report.to_file("templates/ours.html")
+
+        return render_template("ours.html")
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run()
---
+++ demp.py
@@ -0,0 +1,3 @@
+list1 = 0
+list1 = [1, 2, 3]
+print(list1)
